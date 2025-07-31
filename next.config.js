48e69009dceb486c3fe68683621d30148086adf0
hellen-Tchovelidze <elenechovelidze@gmail.com>
2025-07-31 21:04:08 +0400

    t

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    reactStrictMode: true,
+    experimental: {
+      serverActions: true, // optional, თუ იყენებ
+    },
+  };
+  
+  module.exports = nextConfig;
+  
